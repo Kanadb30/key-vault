91d2ff1d2b0ae7ae1c7ccb1643875fe2767c3c5a
Kanadb30 <kanadb@iitbhilai.ac.in>
2026-08-07 06:33:57 +0000

    Added comts to errors

--- src/customErrors.rs
+++ src/customErrors.rs
@@ -1,5 +1,9 @@
 use thiserror::Error;
 
+// Its used so that Result<T, E> can now cover String as well as I/O error at the same time.
+// plus dont need to write Result<T, CustomE> again and again. Just use Result<T> now.
+
+
 pub type Result<T> = std::result::Result<T, CustomError>;
 
 #[derive(Error, Debug)]
