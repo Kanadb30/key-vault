60518326ef7f1db4492b04a4e0824aa18e002d17
Kanadb30 <kanadb@iitbhilai.ac.in>
2026-08-12 14:44:42 +0000

    refactor: added vec impl of LRU and evict and flush methods

--- src/BufferPool.rs
+++ src/BufferPool.rs
@@ -1,12 +1,16 @@
 use crate::page::{Page, PAGE_SIZE};
 use crate::diskManager::DiskManager;
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 use crate::customErrors::{CustomError, Result};
 
+pub const BUFFER_POOL_CAPACITY: usize = 5; // aise hi 5 set hai -> will change later to dynamic value according to system specific needs.
+// current implementation uses vec for LRU cache but will change it to HashMap<Page_no, Node> and doublyLinkedList in future implementation.
 pub struct BufferPool {
     dm: DiskManager,
     page_table: HashMap<u32, Page>,
     dirty_pages: HashSet<u32>,
+    capacity: usize,
+    lru: VecDeque<u32>,
 }
 
 impl BufferPool {
@@ -22,6 +26,8 @@ impl BufferPool {
             dm: disk_manager,
             page_table: HashMap::new(),
             dirty_pages: HashSet::new(),
+            capacity: BUFFER_POOL_CAPACITY,
+            lru: VecDeque::new(),
         })
     }
 
@@ -29,18 +35,37 @@ impl BufferPool {
         if !self.page_table.contains_key(&page_no) {
             let page = self.dm.read_page(page_no)?;
             self.page_table.insert(page_no, page);
+            self.lru.push_front(page_no);
+            self.check_for_flush()?;
+            return Ok(self.page_table.get(&page_no).unwrap());
+        } else{
+            self.lru.retain(|&existing_page_no| existing_page_no != page_no);
+            self.lru.push_front(page_no);
+            self.check_for_flush()?;
+            return Ok(self.page_table.get(&page_no).unwrap());
         }
-        Ok(self.page_table.get(&page_no).unwrap())
     }
+
+    
 
     pub fn fetch_page_mut(&mut self, page_no: u32) -> Result<&mut Page> {
         if !self.page_table.contains_key(&page_no) {
             let page = self.dm.read_page(page_no)?;
             self.page_table.insert(page_no, page);
+            self.dirty_pages.insert(page_no);
+            self.lru.push_front(page_no);
+            self.check_for_flush()?;
+            return Ok(self.page_table.get_mut(&page_no).unwrap());
+        }else {
+            self.dirty_pages.insert(page_no);
+            self.lru.retain(|&existing_page_no| existing_page_no != page_no);
+            self.lru.push_front(page_no);
+            return Ok(self.page_table.get_mut(&page_no).unwrap());
         }
-        self.dirty_pages.insert(page_no);
-        Ok(self.page_table.get_mut(&page_no).unwrap())
     }
+
+    // flush all changes to disk and clear dirt page set.
+    // NOTE : -> IT doesnt affect LRU OR available page map in buufer.
 
     pub fn flush(&mut self) -> Result<()> {
         for page_no in &self.dirty_pages {
@@ -48,6 +73,25 @@ impl BufferPool {
             self.dm.write_page(*page_no, &page.data)?;
         }
         self.dirty_pages.clear();
+        Ok(())
+    }
+
+    // Evicts the least recently used page from the buffer -> capacity exceeded case.
+
+    fn check_for_flush(&mut self) -> Result<()> {
+        if(self.lru.len() > self.capacity) {
+            let pop_page_no = self.lru.pop_back().unwrap();
+            if self.dirty_pages.contains(&pop_page_no) {
+                let page = self.page_table.get(&pop_page_no).unwrap();
+                self.dm.write_page(pop_page_no, &page.data)?;
+                self.dirty_pages.remove(&pop_page_no);
+            }
+            if self.dirty_pages.contains(&pop_page_no) {
+                self.dm.write_page(pop_page_no, &self.page_table.get(&pop_page_no).unwrap().data)?;
+                self.dirty_pages.remove(&pop_page_no);
+            }
+            self.page_table.remove(&pop_page_no);
+        }
         Ok(())
     }
 }
