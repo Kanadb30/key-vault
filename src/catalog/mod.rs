396ce8b0e18cc8bd6bd2e2892736eddf4fc6032b
Kanadb30 <kanadb@iitbhilai.ac.in>
2026-08-17 07:34:07 +0000

    added required imports and uses

---
+++ src/catalog/mod.rs
@@ -0,0 +1,6 @@
+pub mod metadata;
+pub mod table;
+
+pub use metadata::DatabaseMetadata;
+pub use table::TableId;
+pub use table::TableMetadata;
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod error;
 pub mod storage;
+pub mod catalog;
 
 pub use crate::error::{CustomError, Result};
 pub use crate::storage::buffer_pool::{BufferPool, BUFFER_POOL_CAPACITY};
